a16ef1f3df6e2a62797f650c2f758e66a95dd9e6
acuevasd <acuevasd@chicagobooth.edu>
2023-03-09 03:57:49 +0000

    not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,8 @@ class ApplicationController < ActionController::Base
     request.format.json?
   end
 
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+  
 end
